c88dbb872b7ea24afc8d7d7bbf345a1959a5063c
RodrigoDornelles <rodrigo@dornelles.me>
2021-01-13 00:08:03 -0300

    adjusts extra small screens

--- _includes/partners.rb
+++ _includes/partners.rb
@@ -1,7 +1,7 @@
 <div class="row">
 {% for parceiro in site.pages %} 
 {% if parceiro.dir contains "parceiro" %}
-<a class="col s3 tooltipped" href="{{ parceiro.url }}" style="display:block" data-position="top" data-tooltip="{{ parceiro.title }}">
+<a class="col s6 m3 tooltipped" href="{{ parceiro.url }}" style="display:block" data-position="top" data-tooltip="{{ parceiro.title }}">
     <img src="{{ parceiro.image }}" alt="{{ parceiro.title }}" style="width:100%"/>
 </a>
 {% endif %}
--- _includes/soluctions.rb
+++ _includes/soluctions.rb
@@ -1,7 +1,7 @@
 <section class="row catalago-row">
 {% for catalago in site.pages %} 
 {% if catalago.filter %}
-<div class="col s6 l3 catalago-item">
+<div class="col xs12 s6 l3 catalago-item">
 <a href="{{ catalago.url }}" tile="Veja o catalago!">
     <i class="large material-icons">{{ catalago.icon }}</i>
     <h5>{{ catalago.title }}</h5>
